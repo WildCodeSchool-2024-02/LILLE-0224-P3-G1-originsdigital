19ec7b6f65204f7a1492ceff1b8c6c7683fe2044
alexandre michiels <alexoualexandre1@gmail.com>
2024-06-14 11:06:37 +0200

    compte en travaux

---
+++ client/src/components/Compte/Compte.jsx
@@ -0,0 +1,12 @@
+import { Form } from "react-router-dom";
+import "./Compte.css";
+
+function Compte() {
+  return (
+    <Form className="form-mon-compte">
+      <h1 className="h1-mon-compte">Modifier mon profil</h1>
+    </Form>
+  );
+}
+
+export default Compte;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,19 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import Compte from "./components/Compte/Compte";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/compte",
+    element: <Compte />,
+    // action: (req,_,_)=>{
+    //   console.info(req.body)
+    // }
   },
 ]);
 
