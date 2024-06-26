f93c2a9cb9053a3b0edba4962e0892a846f171e9
Your Name <you@example.com>
2024-06-26 10:46:17 +0200

    reparation css et creation page browser

---
+++ client/src/pages/PageBrowse.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header/header";
+import VideoCard from "../components/VideoCard/VideoCard";
+import Footer from "../components/Footer";
+
+function PageBrowse() {
+  return (
+    <>
+      <Header />
+      <VideoCard />
+      <Footer />
+    </>
+  );
+}
+
+export default PageBrowse;
