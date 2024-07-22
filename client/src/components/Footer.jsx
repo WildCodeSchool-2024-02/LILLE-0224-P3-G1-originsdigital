08bd9e6a67cf05c62de4159ab918274ca32fabf8
alexandre michiels <alexoualexandre1@gmail.com>
2024-07-22 14:42:33 +0200

    fixefooter

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,9 +4,15 @@ function Footer() {
   return (
     <div className="footer-jsx">
       {" "}
-      <img src="/public/instagram.svg" alt="teste" className="img-footer" />
-      <img src="/public/facebook.svg" alt="teste" className="img-footer" />
-      <img src="/public/a-propos.png" alt="teste" className="img-footer" />
+      <a href="https://www.instagram.com/">
+        <img src="/public/instagram.svg" alt="teste" className="img-footer" />
+      </a>
+      <a href="https://fr-fr.facebook.com/">
+        <img src="/public/facebook.svg" alt="teste" className="img-footer" />
+      </a>
+      <a href="/info-contact#hdp">
+        <img src="/public/a-propos.png" alt="teste" className="img-footer" />
+      </a>
     </div>
   );
 }
